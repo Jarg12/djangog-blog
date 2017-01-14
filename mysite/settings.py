3c8a91b4d28574b8788d0ac35ccbe71a997433b4
jarg <jonharg@gmail.com>
2017-01-14 21:57:40 +0000

    User logon and logout added

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,6 +18,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
+LOGIN_REDIRECT_URL = '/'
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'mamuldnws00p&trw*_wt@s%9tuwwxyfo$v4q64ccmswm!o*^0g'
